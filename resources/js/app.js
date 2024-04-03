da48781776bbfee90cd7363925a041a14fc645b6
Sonya <SofiKonors@gmail.com>
2024-04-03 15:12:29 +0300

    4-10 category filtering

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,10 @@ import './bootstrap';
 
 import { createApp } from 'vue'
 import PostsIndex from './components/Posts/Index.vue'
+import { Bootstrap4Pagination } from 'laravel-vue-pagination';
+import { Bootstrap5Pagination } from 'laravel-vue-pagination';
+import { TailwindPagination } from 'laravel-vue-pagination';
+
 
 createApp({})
     .component('PostsIndex', PostsIndex)
