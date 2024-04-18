1a12c5343896d388e97194376a05b7e2ecf77365
Sonya <SofiKonors@gmail.com>
2024-04-18 12:18:52 +0300

    done routes

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,13 @@
 import './bootstrap';
 
 import { createApp } from 'vue'
-import PostsIndex from './components/Posts/Index.vue'
+import App from './layouts/App.vue' 
+import router from './routes/index' 
 import { Bootstrap4Pagination } from 'laravel-vue-pagination';
 import { Bootstrap5Pagination } from 'laravel-vue-pagination';
 import { TailwindPagination } from 'laravel-vue-pagination';
 
 
-createApp({})
-    .component('PostsIndex', PostsIndex)
+createApp(App) 
+    .use(router)
     .mount('#app')
